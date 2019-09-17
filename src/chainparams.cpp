62f6762f64f0d030141593a3d7b0a8bf638afbc2
benjioh5 <benjioh5@gmail.com>
2019-09-18 00:09:13 +0900

    fix maturity

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -244,7 +244,7 @@ public:
         nToCheckBlockUpgradeMajority = 10800; // Approximate expected amount of blocks in 7 days (1440*7.5)
         nMinerThreads = 0;
         nTargetSpacing = 1 * 60;        // 1 minute
-        nMaturity = 100;
+        nMaturity = 10;
         nStakeMinDepth = 600;
         nFutureTimeDriftPoW = 7200;
         nFutureTimeDriftPoS = 180;
