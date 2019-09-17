4acf4db87b6969eb05fe2a63875afa457fa6be42
benjioh5 <benjioh5@gmail.com>
2019-09-18 00:09:31 +0900

    remove network ID check

--- src/init.cpp
+++ src/init.cpp
@@ -1384,7 +1384,7 @@ bool AppInit2()
     // ********************************************************* Step 7: load block chain
 
     //ABLE: Load Accumulator Checkpoints according to network (main/test/regtest)
-    assert(AccumulatorCheckpoints::LoadCheckpoints(Params().NetworkIDString()));
+    //assert(AccumulatorCheckpoints::LoadCheckpoints(Params().NetworkIDString()));
 
     fReindex = GetBoolArg("-reindex", false);
 
